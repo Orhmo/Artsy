39faf7035aa8f7a2e4696c3fd33cbdafe41eee7b
Valentina <oorhmo@gmail.com>
2023-05-03 13:16:59 +0100

    Marketplace Page Layout

--- pages/components/marketplace-layout/filter.tsx
+++ pages/components/marketplace-layout/filter.tsx
@@ -7,45 +7,59 @@ interface Props {
 }
 
 const Filter: React.FC<Props> = ({ onFilter }) => {
-  const categories: string[] = Array.from(new Set(ARTWORKS.map((artwork: Artwork) => artwork.category)));
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-
-  const handleCategoryChange = (category: string): void => {
-    const index = selectedCategories.indexOf(category);
-    if (index === -1) {
-      setSelectedCategories([...selectedCategories, category]);
-    } else {
-      setSelectedCategories([...selectedCategories.slice(0, index), ...selectedCategories.slice(index + 1)]);
-    }
-  };
+    const [selectedCategories, setSelectedCategories] = useState<Array<string>>(['All']);
     
+    const categories: Array<string> = ['All', ...new Set(ARTWORKS.map(artwork => artwork.category))];
+   
+    const handleCategoryChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const category = event.target.value;
+        const newSelectedCategories = [...selectedCategories];
+    
+        if (category === 'All') {
+          // If "All" category is selected, uncheck all other categories
+          newSelectedCategories.splice(1);
+        } else {
+          // If any other category is selected, uncheck the "All" category
+          const allIndex = newSelectedCategories.indexOf('All');
+          if (allIndex !== -1) {
+            newSelectedCategories.splice(allIndex, 1);
+          }
+        }
+    
+        if (newSelectedCategories.includes(category)) {
+          // Remove the category if it's already selected
+          newSelectedCategories.splice(newSelectedCategories.indexOf(category), 1);
+        } else {
+          // Add the category if it's not already selected
+          newSelectedCategories.push(category);
+        }
+    
+        if (newSelectedCategories.length === 0) {
+          // If no category is selected, recheck the "All" category
+          newSelectedCategories.push('All');
+        }
+    
+        setSelectedCategories(newSelectedCategories);
+      };
   
 
   return (
-    <div className='w-full flex justify-center'>
-      <div className='flex items-center justify-between w-full max-w-lg p-4 bg-white shadow rounded-lg'>
-        <div className='flex items-center space-x-4'>
-          <span>Filter by category:</span>
-          {categories.map((category: string) => (
-            <div key={category} className='flex items-center space-x-2'>
-              <input
-                type='checkbox'
-                id={`category_${category}`}
-                name={`category_${category}`}
-                value={category}
-                checked={selectedCategories.includes(category)}
-                onChange={() => handleCategoryChange(category)}
-                className='form-checkbox h-5 w-5 text-blue-600 rounded'
-              />
-              <label htmlFor={`category_${category}`} className='font-medium text-gray-700'>
-                {category}
-              </label>
-            </div>
-          ))}
-        </div>
-        
-      </div>
-    </div>
+    <div className='flex justify-center gap-4 mb-4'>
+    {categories.map(category => (
+      <label key={category}>
+        <input
+          type='checkbox'
+          value={category}
+          checked={selectedCategories.includes(category)}
+                onChange={handleCategoryChange}
+                className=''
+               
+        />
+       {category}
+      </label>
+    ))}
+  </div>
+
   );
 };
 
