b982159afe4c51b856e7a1fff1a2368af7ad1ff8
Valentina <oorhmo@gmail.com>
2023-05-03 10:52:42 +0100

    Marketplace Page Layout

---
+++ pages/marketplace.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+import NavBar from './components/navBar'
+import Footer from './components/footer'
+
+import { ARTWORKS } from '../utils/data';
+
+const Marketplace = () => {
+  const [displayCount, setDisplayCount] = useState(9);
+  const totalArtworks = ARTWORKS.length;
+
+  const displayedArtworks = ARTWORKS.slice(0, displayCount);
+  const displayedCountText = `See 1-${displayCount} of ${totalArtworks} results`;
+
+  const handleSeeMoreClick = () => {
+    setDisplayCount(displayCount + 3);
+  };
+
+  return (
+      <div className='flex flex-col justify-center items-center my-4'>
+          <div className='text-center mt-4 px-8 py-2 border rounded-lg shadow-lg mb-8'>{displayedCountText}</div>
+
+      <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 md:gap-12'>
+        {displayedArtworks.map((artwork) => (
+          <div
+            className='mx-auto flex flex-col flex-wrap justify-center gap-2 rounded-2xl p-4 shadow-2xl shadow-blue-50'
+            key={artwork.id}
+          >
+            <Image src={artwork.image} alt='' />
+            <h2 className='py-4 uppercase italic'>{artwork.name}</h2>
+            <span className='py-2 font-bold'>{artwork.price}</span>
+          </div>
+        ))}
+      </div>
+      {displayCount < totalArtworks && (
+        <div className='border-2 px-10 py-4 rounded-lg mt-16 cursor-pointer hover:bg-blue-50 hover:text-gray-500 transition-all'>
+          <button onClick={handleSeeMoreClick}>See more</button>
+        </div>
+      )}
+      
+    </div>
+  );
+};
+
+export default Marketplace;
---
+++ utils/data.tsx
@@ -0,0 +1,88 @@
+import { nanoid } from "nanoid";
+
+import Artwork_1 from '/public/images/marketplace/Artwork_1.svg'
+import Artwork_2 from '/public/images/marketplace/Artwork_2.svg'
+import Artwork_3 from '/public/images/marketplace/Artwork_3.svg'
+import Artwork_4 from '/public/images/marketplace/Artwork_4.svg'
+import Artwork_5 from '/public/images/marketplace/Artwork_5.svg'
+import Artwork_6 from '/public/images/marketplace/Artwork_6.svg'
+import Artwork_7 from '/public/images/marketplace/Artwork_7.svg'
+import Artwork_8 from '/public/images/marketplace/Artwork_8.svg'
+import Artwork_9 from '/public/images/marketplace/Artwork_9.svg'
+
+
+
+export const ARTWORKS = [
+    {
+        id: nanoid(),
+        image: Artwork_1,
+        name: "Philomena'22",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_2,
+        name: "Boolean Egyptian",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_3,
+        name: "Blanc",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_4,
+        name: "Ellipsia",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_5,
+        name: "The Lawmarkers",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_6,
+        name: " Veil",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_7,
+        name: "Alternating",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_8,
+        name: "Rosemary'22",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_9,
+        name: "Beverly",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_1,
+        name: "Alternating",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_2,
+        name: "Rosemary'22",
+        price: "$3.90",
+    },
+    {
+        id: nanoid(),
+        image: Artwork_3,
+        name: "Beverly",
+        price: "$3.90",
+    },
+]
